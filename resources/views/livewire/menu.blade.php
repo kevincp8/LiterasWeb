dc6a20d8ee03ea357093028bc3f3438ac7aa9513
kevincp8 <kevinc8@gmail.com>
2024-10-09 20:31:46 +0200

    feat: barra de navegacion y pagina principal

---
+++ resources/views/livewire/menu.blade.php
@@ -0,0 +1,26 @@
+<div>
+  @vite('resources/css/app.css')
+  <menu>
+    <nav class="bg-stone-200">
+      <li class="flex p-6 font-mono text-xl font-bold">
+        <ul class="invisible ml-auto mr-36 sm:visible">
+          <a wire:click="redirectProductos()" class="cursor-pointer">{{__("Productos")}}</a>
+        </ul>
+        <ul class="invisible mr-36 sm:visible">
+          <a wire:click="redirectInformacion()" class="cursor-pointer">{{__("Información")}}</a>
+          
+        </ul>
+        <ul class="invisible mr-36 sm:visible">
+          <a wire:click="redirectContacto()" class="cursor-pointer">{{__("Contacto")}}</a>
+        </ul>
+        <ul class="visible absolute right-0 mr-5 sm:invisible">
+          <button class="group pointer-events-auto cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 transition-transform duration-500 group-active:rotate-90">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </ul>
+      </li>
+    </nav>
+  </menu>
+</div>
